0be021cd6a561e80af29fe6d38660fba9d7aba5b
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-02-23 00:07:04 +0530

    camera controller file

--- src/controller/camera.controller.js
+++ src/controller/camera.controller.js
@@ -1,24 +1,12 @@
 const express=require("express")
 
-const Product=require("../models/product.model")
+const Product=require("../models/camera.model")
 
 const router=express.Router()
 
-router.get("/brand",async(req,res)=>{
+const allcontroller=require("./allcontroller")
 
-    try{
-        console.log(req.query.brand)
-
-        const product=await Product.find({manufacturer:req.query.brand}).sort({price:-1}).limit(15).lean().exec()
-        
-
-        res.send(product)
-
-    }   
-    catch(er){
-        console.log(er.message)
-    }
-})
+router.get("/brand",allcontroller(Product).brand)
 
 router.get("",async(req,res)=>{
 
