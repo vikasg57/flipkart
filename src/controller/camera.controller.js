b68495076724cb1055eac3322bdb645a599921ab
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-02-23 20:42:00 +0530

    rotes added in allcontroller

--- src/controller/camera.controller.js
+++ src/controller/camera.controller.js
@@ -8,45 +8,9 @@ const allcontroller=require("./allcontroller")
 
 router.get("/brand",allcontroller(Product).brand)
 
-router.get("",async(req,res)=>{
+router.get("",allcontroller(Product).get)
 
-    try{
-
-        const product=await Product.find().limit(15).lean().exec()
-        
-
-        res.send(product)
-
-    }   
-    catch(er){
-        console.log(er.message)
-    }
-})
-
-router.get("/sort",async(req,res)=>{
-
-    try{
-
-        if(req.query.value==-1){
-
-                const product=await Product.find().sort({price:req.query.value}).limit(15).lean().exec()
-
-                   res.send(product)
-
-        }
-
-        const product=await Product.find().sort({price:req.query.value}).skip(15).limit(15).lean().exec()
-        
-
-        res.send(product)
-
-    }   
-    catch(er){
-        console.log(er.message)
-    }
-})
-
-
+router.get("/sort",allcontroller(Product).sort)
 
 
 module.exports=router
