3cfa095a81a6650deba23efcde1c176abcdb827a
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-02-23 20:35:50 +0530

    all route in one place

--- src/controller/allcontroller.js
+++ src/controller/allcontroller.js
@@ -15,47 +15,48 @@ const brand=(Model)=>async(req,res)=>{
     }
 }
 
-// router.get("",async(req,res)=>{
+const get=(Model)=>async(req,res)=>{
 
-//     try{
+    try{
 
-//         const product=await Product.find().limit(15).lean().exec()
+        const product=await Model.find().limit(15).lean().exec()
+        
+        res.send(product)
+
+    }   
+    catch(er){
+        console.log(er.message)
+    }
+}
+
+const sort=(Model)=>async(req,res)=>{
+
+    try{
+
+        if(req.query.value==-1){
+
+                const product=await Model.find().sort({price:req.query.value}).limit(15).lean().exec()
+
+                   res.send(product)
+
+        }
+
+        const product=await Model.find().sort({price:req.query.value}).skip(15).limit(15).lean().exec()
         
 
-//         res.send(product)
+        res.send(product)
 
-//     }   
-//     catch(er){
-//         console.log(er.message)
-//     }
-// })
-
-// router.get("/sort",async(req,res)=>{
-
-//     try{
-
-//         if(req.query.value==-1){
-
-//                 const product=await Product.find().sort({price:req.query.value}).limit(15).lean().exec()
-
-//                    res.send(product)
-
-//         }
-
-//         const product=await Product.find().sort({price:req.query.value}).skip(15).limit(15).lean().exec()
-        
-
-//         res.send(product)
-
-//     }   
-//     catch(er){
-//         console.log(er.message)
-//     }
-// })
+    }   
+    catch(er){
+        console.log(er.message)
+    }
+}
 
 module.exports=(Model)=>{
     return{
-        brand:brand(Model)
+        brand:brand(Model),
+        get:get(Model),
+        sort:sort(Model)
     }
 }
 
